c12cc25b4f17bb11e304e3f7243d41b7055be7af
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2022-12-22 10:14:21 +0100

    Web3 Server

---
+++ engine.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res, next) => {
+    //res.sendStatus(200);
+    res.send("Hello world!");
+    res.end()
+});
+
+app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`Server listening on port ${port}`);
+});
